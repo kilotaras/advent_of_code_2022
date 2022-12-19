2689f75213bef26944238142067be3dc4caf8982
Taras Boiko <me@tboiko.com>
2022-12-19 16:15:47 +0000

    d7

--- d7/src/main.rs
+++ d7/src/main.rs
@@ -1,3 +1,6 @@
+use id_tree::*;
+use std::cell::Cell;
+use std::collections::HashMap;
 use std::io::BufRead;
 
 #[derive(Debug, Eq, PartialEq)]
@@ -118,17 +121,99 @@ fn get_commands() -> Vec<Command> {
     // there's no rsplit_inclusive in std, so we reverse the lines and then reverse them back after split
     input_lines.reverse();
 
-    let mut commands = input_lines.split_inclusive(|line| line.starts_with("$")).map(|command_lines| {
-        let mut command_lines = command_lines.to_vec();
-        command_lines.reverse();
-        parse_command(&command_lines)
-    }).collect::<Vec<_>>();
+    let mut commands = input_lines
+        .split_inclusive(|line| line.starts_with("$"))
+        .map(|command_lines| {
+            let mut command_lines = command_lines.to_vec();
+            command_lines.reverse();
+            parse_command(&command_lines)
+        })
+        .collect::<Vec<_>>();
     commands.reverse();
     commands
 }
 
+#[derive(Debug, Eq, PartialEq)]
+enum NodeType {
+    Directory,
+    File,
+}
+
+struct NodeData {
+    size: Cell<Option<usize>>,
+    node_type: NodeType,
+}
+
 fn main() {
+    use id_tree::InsertBehavior::*;
     let commands = get_commands();
 
-    dbg!(commands);
+    let mut tree: Tree<NodeData> = {
+        let cell = Cell::new(None);
+        let data = NodeData {
+            size: cell,
+            node_type: NodeType::Directory,
+        };
+        TreeBuilder::new().with_root(Node::new(data)).build()
+    };
+
+    let root = tree.root_node_id().unwrap().clone();
+    let mut cur_node_id = root.clone();
+    let mut parent_to_child: HashMap<(NodeId, String), NodeId> = HashMap::new();
+
+    for command in commands {
+        match command {
+            Command::Change { to } => {
+                cur_node_id = match to.as_str() {
+                    "/" => root.clone(),
+                    ".." => tree.get(&cur_node_id).unwrap().parent().unwrap().clone(),
+                    _ => parent_to_child.get(&(cur_node_id, to)).unwrap().clone(),
+                }
+            }
+            Command::Ls { entries } => {
+                for entry in entries {
+                    let size = match entry.answer_type {
+                        LsEntryType::Directory => None,
+                        LsEntryType::File(size) => Some(size),
+                    };
+                    let nodeType = match entry.answer_type {
+                        LsEntryType::Directory => NodeType::Directory,
+                        LsEntryType::File(_) => NodeType::File,
+                    };
+                    let nodeData = NodeData {
+                        size: Cell::new(size),
+                        node_type: nodeType,
+                    };
+                    let node = Node::new(nodeData);
+                    let node_id = &mut tree.insert(node, UnderNode(&cur_node_id)).unwrap();
+                    parent_to_child.insert((cur_node_id.clone(), entry.name), node_id.clone());
+                }
+            }
+        }
+    }
+
+    let nodes = tree.traverse_post_order(&root).unwrap();
+
+    let mut total = 0;
+
+    for node in nodes {
+        let data = node.data().size.get();
+
+        if data.is_none() {
+            let size = node
+                .children()
+                .iter()
+                .map(|child_id| tree.get(child_id).unwrap().data())
+                .map(|data| data.size.get().unwrap())
+                .sum::<usize>();
+
+            node.data().size.set(Some(size));
+        }
+        let size = node.data().size.get().unwrap();
+        if size <= 100000 && node.data().node_type == NodeType::Directory {
+            total += size;
+        }
+    }
+
+    println!("{}", total);
 }
